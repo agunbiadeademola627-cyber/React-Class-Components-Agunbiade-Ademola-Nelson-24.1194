2c45b8ae13cce20351bea2191ab772f723e69bfe
agunbiadeademola627-cyber <agunbiadeademola627@gmail.com>
2025-10-05 09:37:55 +0100

    Update Heading.js

--- reactclass/src/components/Heading.js
+++ reactclass/src/components/Heading.js
@@ -1,19 +1,12 @@
 import React from 'react';
 
 function Heading(){
-
-    const borderz = {
-        border: '1px solid red',
-        color: 'green',
-        backgroundColor: 'YELLOW'
-
-    };
-
     return (
-        <div style={borderz}>
+        <div>
             <h1>BU-COS 209 CLASS</h1>
         </div>
     );
 }
+
 
 export default Heading;

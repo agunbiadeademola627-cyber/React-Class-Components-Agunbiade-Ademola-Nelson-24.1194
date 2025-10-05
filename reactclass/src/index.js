00959c5e4c2431444b7f74816702dc145cd3f774
agunbiadeademola627-cyber <agunbiadeademola627@gmail.com>
2025-10-05 09:36:30 +0100

    Update index.js

--- reactclass/src/index.js
+++ reactclass/src/index.js
@@ -2,23 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
-import About from './components/About';
-import Products from './components/Products';
-import Contact from './components/Contact';
-import Heading  from "./components/Heading";
-
-
 const root = ReactDOM.createRoot(document.getElementById('sophia'));
 root.render(
     <React.StrictMode>
-        <App/>
-
-        <h1>Presentation Class</h1>
-        <Heading/>
-        <About/>
-        <Products/>
-        <Contact/>
-
+            <App/>
     </React.StrictMode>
 );
 

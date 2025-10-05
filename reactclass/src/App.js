cdae7e92abdd68e39bb6080ef6c48f8d622c1242
agunbiadeademola627-cyber <agunbiadeademola627@gmail.com>
2025-10-05 09:32:20 +0100

    Update App.js

--- reactclass/src/App.js
+++ reactclass/src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 //APP component
+import About from './components/About';
+import Products from './components/Products';
+import Contact from './components/Contact';
+import Heading  from "./components/Heading";
+
 function App() {
   return (
+    <div>
    <h2>This is my react app with 4 components</h2>
+   <h1>Presentation Class</h1>
+   <Heading/>
+   <About/>
+   <Products/>
+   <Contact/>
+    </div>
   );
 }
 
